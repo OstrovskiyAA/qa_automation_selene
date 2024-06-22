d841ded4d51ae3d9a869bc531d5d4b9598afc08c
Aleksei Ostrovskii <a.a.ostrovskiy@mail.ru>
2024-06-22 12:23:53 +0300

    Added more allure steps

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -17,14 +17,17 @@ class RegistrationPage:
     def fill_first_name(self, first_name="Alexey"):
         browser.element("[id=firstName]").should(be.blank).type(first_name)
 
+    @allure.step("Insert last name")
     def fill_last_name(self, last_name="Ostrovskiy"):
         browser.element("#lastName").should(be.blank).perform(
             command=js.set_value(last_name)
         )
 
+    @allure.step("Insert email")
     def fill_email(self, email="a.a.ostrovskiy@mail.ru"):
         browser.element("[id=userEmail]").should(be.blank).type(email)
 
+    @allure.step("Insert gender")
     def select_gender(self, gender="Male"):
         browser.all("[name=gender]").element_by(have.value(gender)).element(
             ".."
@@ -33,11 +36,13 @@ class RegistrationPage:
         # male.perform(command=js.click)
         # browser.element("[name=gender][value=Male]").click()
 
+    @allure.step("Insert mobile number")
     def fill_mobile_number(self, phone_nimber="8911277596"):
         browser.all("[id^=userNumb]")[2].should(be.blank).with_(
             set_value_by_js=True
         ).set_value(phone_nimber)
 
+    @allure.step("Insert date of birth")
     def fill_date(self, day=30, month=5, year=1992):
         browser.element('[id="dateOfBirthInput"]').should(be.visible).click()
         # ^ - начинается с, ~ - то что такое точно есть в тексте с классом точка означает тоже самое
@@ -48,17 +53,21 @@ class RegistrationPage:
         browser.all(f".react-datepicker__day--0{day}")[1].click()
         # with_ - используется для разовой настройки
 
+    @allure.step("Insert date of birth")
     def fill_date_by_your_own(self, day=30, month="May", year=1992):
         browser.element('[id="dateOfBirthInput"]').should(be.visible).perform(
             command=select_all
         ).type(f"{day} {month} {year}").press_enter()
 
+    @allure.step("Insert subject")
     def fill_subject(self, subject="Computer Science"):
         browser.element("#subjectsInput").type(subject).press_enter()
 
+    @allure.step("Insert hobby")
     def fill_hobby(self):
         browser.all(".custom-checkbox").element_by(have.text("Music")).click()
 
+    @allure.step("Download file")
     # reading = browser.element('[id="hobbies-checkbox-2"]').should(be.present)
     # reading.perform(command=js.click)
     def dowload_file(self):
@@ -68,15 +77,18 @@ class RegistrationPage:
             )
         )
 
+    @allure.step("Insert address")
     def fill_address(self, address="Дачный проспект"):
         browser.element('[id="currentAddress"]').should(be.blank).type(address)
 
+    @allure.step("Insert state")
     def fill_state(self, state="NCR"):
         browser.element("#state").perform(command=js.scroll_into_view).click()
         browser.all("[id^=react-select][id*=option]").element_by(
             have.exact_text(state)
         ).click()
 
+    @allure.step("Insert city")
     def fill_city(self, city="Gurgaon"):
         browser.element(".css-1wa3eu0-placeholder").should(be.present).should(
             have.text("Select City")
--- tests/test_full_form.py
+++ tests/test_full_form.py
@@ -20,32 +20,35 @@ registration_page = RegistrationPage()
 @allure.feature("new tasks")
 @allure.link("https://demoqa.com", name="Testing")
 def test_student_registrate(open_browser):
-    registration_page.open()
-    registration_page.fill_first_name()
-    registration_page.fill_last_name()
-    registration_page.fill_email()
-    registration_page.select_gender()
-    registration_page.fill_mobile_number()
-    # registration_page.fill_date()
-    registration_page.fill_date_by_your_own(30, "May", 1992)
-    registration_page.fill_subject()
-    registration_page.fill_hobby()
-    registration_page.dowload_file()
-    registration_page.fill_address()
-    registration_page.fill_state()
-    registration_page.fill_city()
-    registration_page.submit()
-    registration_page.should_have_registered(
-        "Alexey",
-        "Ostrovskiy",
-        "a.a.ostrovskiy@mail.ru",
-        "Male",
-        "8911277596",
-        "30 May,1992",
-        "Computer Science",
-        "Music",
-        "me.jpg",
-        "Дачный проспект",
-        "NCR",
-        "Gurgaon",
-    )
+    with allure.step("registration"):
+        registration_page.open()
+        registration_page.fill_first_name()
+        registration_page.fill_last_name()
+        registration_page.fill_email()
+        registration_page.select_gender()
+        registration_page.fill_mobile_number()
+        # registration_page.fill_date()
+        registration_page.fill_date_by_your_own(30, "May", 1992)
+        registration_page.fill_subject()
+        registration_page.fill_hobby()
+        registration_page.dowload_file()
+        registration_page.fill_address()
+        registration_page.fill_state()
+        registration_page.fill_city()
+    with allure.step("submit"):
+        registration_page.submit()
+    with allure.step("assertion"):
+        registration_page.should_have_registered(
+            "Alexey",
+            "Ostrovskiy",
+            "a.a.ostrovskiy@mail.ru",
+            "Male",
+            "8911277596",
+            "30 May,1992",
+            "Computer Science",
+            "Music",
+            "me.jpg",
+            "Дачный проспект",
+            "NCR",
+            "Gurgaon",
+        )
